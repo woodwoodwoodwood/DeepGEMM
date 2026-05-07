22f3821f317536c94abcb16bdc3947c8649e1935
cakejiang <cakejiang@tencent.com>
2026-05-07 17:46:57 +0800

    add run_test_mega_moe.sh

--- run_test_mega_moe.sh
+++ run_test_mega_moe.sh
@@ -3,6 +3,7 @@ set -e
 cd "$(dirname "$0")"
 
 export CUDA_VISIBLE_DEVICES=3,4
+export PYTHONPATH="$(dirname "$0"):$PYTHONPATH"
 
 echo "Running MegaMoE test on GPU 3,4..."
 python tests/test_mega_moe.py --num-processes 2
